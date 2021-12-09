46f1b7b73b22ba915913f19ce856d2947ed6bafe
Nick Darlow <dangerous.darlow@gmail.com>
2021-12-09 12:52:04 +0000

    Implement day 1 part 2

--- Tests.cs
+++ Tests.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 
+// ReSharper disable ParameterTypeCanBeEnumerable.Local
+// ReSharper disable ReturnTypeCanBeEnumerable.Global
+
 namespace AdventOfCode2021;
 
 public class Tests
@@ -11,19 +14,48 @@ public class Tests
     [Theory]
     [InlineData("Puzzle01_Example.txt", 7)]
     [InlineData("Puzzle01.txt", 1121)]
-    public async Task Day01_Part1(string inputPath, int expectedValue)
+    public async Task Day01_Part1(string inputPath, int result)
+    {
+        var values = await ReadToInts(inputPath);
+        CountGreaterThanPrevious(values).Should().Be(result);
+    }
+
+    [Theory]
+    [InlineData("Puzzle01_Example.txt", 5)]
+    [InlineData("Puzzle01.txt", 1065)]
+    public async Task Day01_Part2(string inputPath, int result)
+    {
+        var values = await ReadToInts(inputPath);
+        var windows = values.ToWindows();
+        CountGreaterThanPrevious(windows).Should().Be(result);
+    }
+
+    private static async Task<int[]> ReadToInts(string inputPath)
     {
         var lines = await File.ReadAllLinesAsync(inputPath);
+        return lines.Select(int.Parse).ToArray();
+    }
 
-        var lastDepth = int.MinValue;
-        var deeperThanLast = lines.Select(int.Parse).Count(depth =>
+    private static int CountGreaterThanPrevious(int[] values)
+    {
+        var previous = int.MaxValue;
+        return values.Count(value =>
         {
-            var greaterThanLastDepth = depth > lastDepth;
-            lastDepth = depth;
-            return greaterThanLastDepth;
+            var previousPrevious = previous;
+            previous = value;
+            return value > previousPrevious;
         });
+    }
+}
 
-        // -1 because the first depth doesn't count
-        (deeperThanLast - 1).Should().Be(expectedValue);
+public static class IntArrayExtensions
+{
+    public static int[] ToWindows(this int[] values)
+    {
+        var windows = new int[values.Length - 2];
+        for (var index = 0; index < values.Length - 2; ++index)
+            windows[index] = values[index] + values[index + 1] + values[index + 2];
+
+        return windows;
     }
 }
